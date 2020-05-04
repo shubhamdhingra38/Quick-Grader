c122e98e54930a472b09d8248a3629972e40fc5a
Shubham Dhingra <shubham@Shubhams-MacBook-Air.local>
2020-05-04 13:13:52 +0530

    Added a little bit of UI, crispy forms

--- auto_tests/auto_tests/settings.py
+++ auto_tests/auto_tests/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'teacher',
     'tests',
     'login_platform',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
