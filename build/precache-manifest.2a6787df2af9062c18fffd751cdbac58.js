97c6be803299685e9c3e8ae23205d4367f0b7a39
Shubham <dhingra.shubham38@gmail.com>
2021-04-09 10:47:32 +0530

    Build

---
+++ build/precache-manifest.2a6787df2af9062c18fffd751cdbac58.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "614e0d6e9adc4a0f3a5458ec1ec1d073",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47e518c4cd1924cb4ee7",
+    "url": "/static/css/main.607001ac.chunk.css"
+  },
+  {
+    "revision": "d06a2915ac42db47e593",
+    "url": "/static/js/2.7a2241df.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.7a2241df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47e518c4cd1924cb4ee7",
+    "url": "/static/js/main.dc0b6d40.chunk.js"
+  },
+  {
+    "revision": "d7778145c378f804b44e",
+    "url": "/static/js/runtime-main.94293c76.js"
+  },
+  {
+    "revision": "8247ead27cdd0a4864654441d8e7628d",
+    "url": "/static/media/copyright.8247ead2.png"
+  },
+  {
+    "revision": "183b9fc10abf56d6e4f443ad27d38adb",
+    "url": "/static/media/csv.183b9fc1.png"
+  },
+  {
+    "revision": "7c8b55d78e3b02332709b309e4d016c8",
+    "url": "/static/media/loading.7c8b55d7.png"
+  },
+  {
+    "revision": "b0e6dbb835edd5fb60a2af182d0ecdab",
+    "url": "/static/media/logo.b0e6dbb8.png"
+  },
+  {
+    "revision": "66904178c951507182cac4bcb3b5dd44",
+    "url": "/static/media/student.66904178.jpg"
+  },
+  {
+    "revision": "ffa87314af9d0dcf1242d3bba8a6e41b",
+    "url": "/static/media/teacher.ffa87314.jpg"
+  }
+]);
